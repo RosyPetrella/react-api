b985043608ad41ac80f6cc44d8099c2f590218fb
RosyPetrella <petrellamr@gmail.com>
2025-03-28 17:07:54 +0100

    Add data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+
+const api_url = "http://localhost:3003/api/v1/posts";
 function App() {
-  return <></>;
+  useEffect(() => {
+    fetchData(api_url);
+  }, []);
+
+  function fetchData(url) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
 }
 
 export default App;
